837c53ec6a494740dd3d44a4b1c7bae06f9b211f
gimdongwon <ehddnjs8989@gmail.com>
2023-11-22 20:52:44 +0900

    (feat) client, server default 세팅 완료

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,24 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// const userSchema = mongoose.Schema({
-
-// })
-
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: false },
   age: { type: Number, required: true, unique: false },
   gender: { type: String, required: true, unique: false },
-  // _id: { type: Schema.Types.ObjectId, required: true, unique: true },
+  _id: { type: Schema.Types.ObjectId, required: true, unique: true },
 });
-// userSchema.statics.findAll = function () {
-//   return this.find({});
-// };
-
-// userSchema.statics.create = function (payload) {
-//   const user = new this(payload);
-//   return user.save();
-// };
 
 const User = mongoose.model("User", userSchema);
 
